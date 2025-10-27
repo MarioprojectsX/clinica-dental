cd001a386782afde707920533a8ec89d6fa9f2bc
Daniel Ureta <duretae@gmail.com>
2025-10-27 02:00:03 -0500

    en produccion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';          // ✅ <-- antes: /serverless o /edge
 import vue from '@astrojs/vue';
 
 export default defineConfig({
-  output: 'server',                             // ✅ <-- antes: 'hybrid' (inválido)
-  adapter: vercel(),                            // ✅ <-- sin runtime; el adapter ya elige lo correcto en Vercel
+  output: 'static',                             // ✅ <-- antes: 'hybrid' (inválido)
   integrations: [vue()],
   devToolbar: { enabled: false },
 });
